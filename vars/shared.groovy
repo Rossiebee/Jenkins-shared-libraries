a111d70ab4c44718e6100e7b11233d15972cff54
Rossiebee <118617194+Rossiebee@users.noreply.github.com>
2023-01-28 22:11:30 -0500

    Create shared.groovy

---
+++ vars/shared.groovy
@@ -0,0 +1,23 @@
+//Declarative Pipeline
+@Library('Rossies-sharedlibs') _
+pipeline{
+agent any
+  tools{
+  maven "3.8.6"
+  }
+  stages{
+    stage('gitpull') {
+      steps {
+      sh "echo start of project"
+        git 'https://github.com/Rossiebee/tesla-project'
+      }
+    }
+  }
+    
+    
+    
+         
+     
+
+  }
+
